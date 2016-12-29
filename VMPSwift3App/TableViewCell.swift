063736c0725cfd5f75ae8ec582cd117257f8ce51
NemSothe <nemsothea13@gmail.com>
2016-12-29 20:47:40 +0700

    prepare view and controller

---
+++ VMPSwift3App/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  VMPSwift3App
+//
+//  Created by macOSX on 12/29/16.
+//  Copyright © 2016 macOSX. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
