4f1b43b43ec5badff4b086154fa88b86ba3d49f5
NemSothe <nemsothea13@gmail.com>
2016-12-30 06:54:33 +0700

    alamofire

---
+++ VMPSwift3App/Article.swift
@@ -0,0 +1,22 @@
+//
+//  Article.swift
+//  VMPSwift3App
+//
+//  Created by macOSX on 12/29/16.
+//  Copyright © 2016 macOSX. All rights reserved.
+//
+
+import Foundation
+class Article {
+    var id:Int?
+    var title:String?
+    var description:String?
+    var image:String?
+    init() {
+        self.id = 0
+        self.title = ""
+        self.description = ""
+        self.image = ""
+    }
+    
+}
